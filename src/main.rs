10b365bfc8c31979161634e02e4657724a665c0d
Lynx20wz <maks20wz@gmail.com>
2026-08-22 16:07:14 +0300

    Rename `mute` with `blur` source

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ use obws::{Client, requests::sources::SourceId};
 use std::env::args;
 
 fn main() {
-    let mute_source: Option<String> = args().nth(1);
+    let blur_source: Option<String> = args().nth(1);
 
     let text: Result<String, obws::error::Error> = trpl::block_on(async {
         let client: Client = Client::connect("127.0.0.1", 4455, Some("")).await?;
 
         let current_scene = get_current_scene(&client).await?;
-        let is_muted = is_black_screen_muted(&client, mute_source.as_deref()).await?;
+        let is_blurred = is_blur_source_active(&client, blur_source.as_deref()).await?;
 
         let mut output = current_scene;
-        if is_muted {
-            output.push_str(" (muted)");
+        if is_blurred {
+            output.push_str(" (blurred)");
         }
 
         Ok(output)
@@ -28,15 +28,15 @@ async fn get_current_scene(client: &Client) -> Result<String, obws::error::Error
     Ok(current_scene)
 }
 
-async fn is_black_screen_muted(
+async fn is_blur_source_active(
     client: &Client,
     mute_source: Option<&str>,
 ) -> Result<bool, obws::error::Error> {
     if let Some(mute_source) = mute_source {
         let sources = client.sources();
         let black_screen_id = SourceId::from(mute_source);
-        let is_muted = sources.active(black_screen_id).await?.active;
-        Ok(is_muted)
+        let is_blurred = sources.active(black_screen_id).await?.active;
+        Ok(is_blurred)
     } else {
         Ok(false)
     }
